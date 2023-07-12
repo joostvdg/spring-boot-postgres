b87e5ca7bd6e0307654ef877beee39a5160db754
Joost van der Griendt <joostvdg@gmail.com>
2023-07-12 16:29:12 +0200

    add hello controller

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Hello sayHello() {
+        LOG.info("Getting all users.");
+        return new Hello("Hell world!");
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Hello.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Hello {
+    private final String greeting;
+
+    public Hello(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+}
